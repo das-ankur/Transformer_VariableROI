4b0edcfcaea941c97af2ec44eb4672883c05b00d
Ankur Das <dankur542@gmail.com>
2024-10-05 01:41:57 +0530

    debug v1.6

--- examples/eval_variable.py
+++ examples/eval_variable.py
@@ -227,8 +227,9 @@ def test_epoch(epoch, test_dataloader, model, criterion_rd, metrics, stage='test
                     gen_img = out_net['x_hat'][0].permute(1, 2, 0)
                     gen_img *= 255
                     gen_img = torch.clamp(gen_img, 0, 255)
-                    gen_img = gen_img.to(torch.int)
-                    print(gen_img)
+                    gen_img = gen_img.to(torch.int).numpy()
+                    gen_img = Image.fromarray(gen_img)
+                    gen_img.save(os.path.join('compressed_images/000.png'))
                     exit(1)
 
                     # Save generated images
